79eb147806d864edbe0e970a9f6694b0c5c9aa6f
Jason Sellgren <matrixx333@gmail.com>
2023-12-25 11:17:51 -0700

    simplified class construction

--- src/Services/ShippingCompanyService.cs
+++ src/Services/ShippingCompanyService.cs
@@ -3,16 +3,14 @@ using Microsoft.EntityFrameworkCore;
 
 class ShippingCompanyService(ShippingDb db)
 {
-    private readonly ShippingDb _db = db;
-
     public async Task<List<ShippingCompany>> GetShippingCompaniesAsync()
     {
-        return await _db.ShippingCompanies.ToListAsync();
+        return await db.ShippingCompanies.ToListAsync();
     }
 
     public async Task<ShippingCompany> GetShippingCompanyAsync(int id)
     {
-        var shippingCompany = await _db.ShippingCompanies.FindAsync(id);
+        var shippingCompany = await db.ShippingCompanies.FindAsync(id);
         return shippingCompany ?? throw new Exception("Shipping Company not found.");
     }
 }
